e1771c1ac9c540510c63d6e80ed6524f7e0808c6
matt venn <matt@mattvenn.net>
2019-08-16 16:50:22 +0200

    updated for gui to keep checking serial status

--- light_controller/serial_plug.py
+++ light_controller/serial_plug.py
@@ -6,43 +6,65 @@ RAISE_VIDPID = '1209:2201'
 class SerialPlug():
 
     def __init__(self):
-        self.setup()
+        self.connected = False
+        self.ser = None
+
+    def is_connected(self):
+        return self.connected
+
+    def get_status_message(self):
+        if self.connected:
+            return "connected on port %s" % self.port
+        else:
+            return("trying to connect to a Raise...")
+
+    def check_serial_status(self):
+        if self.connected:
+            # try a write
+            self.run_cmd("", quiet=True)
+        else:
+            # otherwise try to setup
+            self.setup()
 
     def setup(self):
         ports = serial.tools.list_ports.grep(RAISE_VIDPID)
         for port in ports:
             try:
-                logging.info("found %s port on %s" % (port.usb_description(), port.device))
-                self.ser = serial.Serial (port.device, 9600, timeout = 2, write_timeout = 1)
-                time.sleep(0.5) # let it settle
+                logging.info("found Dygma Raise on %s" % port.device)
+                self.ser = serial.Serial(port.device, 9600, timeout = 1, write_timeout = 1)
+                self.port = port.device
+                self.connected = True
                 return True
             except serial.serialutil.SerialException:
+                self.connected = False
                 return False
 
-    def run_cmd(self, cmd):
-
+    def run_cmd(self, cmd, quiet=False):
         output = ""
-        logging.info(cmd)
+        if not quiet:
+            logging.info("> %s" % cmd)
         try:
-            self.ser.write (str.encode(cmd + "\n")) # must write \n with no delay or 10% chance not receive command correctly!
-        except serial.writeTimeoutError:
+            self.ser.write(str.encode(cmd + "\n")) # must write \n with no delay or 10% chance not receive command correctly!
+
+            while True:
+                resultLine = self.ser.readline().decode('utf-8')
+
+                if resultLine == "\r\n" or resultLine == "\n":
+                    resultLine = " "
+                else:
+                    resultLine = resultLine.rstrip()
+
+                if resultLine == ".":
+                    break
+
+                if resultLine:
+                    output += resultLine
+                    if not quiet:
+                        logging.info("< %s" % resultLine)
+
+        except serial.serialutil.SerialException:
             logging.warning("write timeout")
-            return output
+            self.connected = False
 
-        while True:
-            resultLine = self.ser.readline().decode('utf-8')
-
-            if resultLine == "\r\n" or resultLine == "\n":
-                resultLine = " "
-            else:
-                resultLine = resultLine.rstrip ()
-
-            if resultLine == ".":
-                break
-
-            if resultLine:
-                output += resultLine
-
-        logging.info(output)
         return output
 
